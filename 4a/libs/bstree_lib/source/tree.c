ba5008c88f358c59a7c5efa145be87736b3f03b0
onar <swefic@outlook.com>
2026-04-16 02:17:56 +0300

    test

--- 4a/libs/bstree_lib/source/tree.c
+++ 4a/libs/bstree_lib/source/tree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include "tree.h"
 #include "node.h"
@@ -34,24 +35,69 @@ Node *FindKey(Tree *tree, size_t key) {
     return cur;
 }
 
+Node *FindMinKey(Node *node) {
+    if (!node) {
+        return NULL;
+    }
+    Node *cur = node, *prev = NULL;
+    while (cur) {
+        prev = cur;
+        cur = cur->relatives[LEFT]; 
+    }
+    return prev;
+}
+
+Node *FindNextKey(Tree *tree, size_t key) {
+    if (!tree) {
+        return NULL;
+    }
+    Node *cur = tree->root;
+    RelativeIndex index = LEFT;
+    while (cur && cur->key != key) {
+        index = LEFT;
+        if (key >= cur->key) {
+            index = RIGHT;
+        }
+        cur = cur->relatives[index];
+    }
+    if (!cur) {
+        return NULL;
+    }
+    Node *node_possible = FindMinKey(cur->relatives[RIGHT]);
+   /* if (node_possible) {
+        return node_possible;
+    }*/
+    if (!node_possible) {
+        Node *parent = cur->relatives[PARENT];
+        while (parent && cur == parent->relatives[RIGHT]) {
+            cur = parent;
+            parent = parent->relatives[PARENT];
+        }
+        return parent;
+    }
+    return node_possible;
+}
+
 TreeStatus TreeInsert(Tree *tree, size_t key, size_t *info) {
     if (!tree || !info) {
         return NOT_EXIST;
     }
     Node *cur = tree->root, *prev = NULL;
+    RelativeIndex index = LEFT;
     while (cur) {
         prev = cur;
         if (key > cur->key) {
-            cur = cur->right;
+            index = RIGHT;
         } else if (key < cur->key) {
-            cur = cur->left;
+            index = LEFT;
         } else { 
             while (cur && key == cur->key) {
                 prev = cur;
-                cur = cur->right;
+                cur = cur->relatives[RIGHT];
             }
             break;
         }
+        cur = cur->relatives[index];
     }
     Node *temp = NodeCreate(prev, key, info);
     if (!temp) {
@@ -62,15 +108,12 @@ TreeStatus TreeInsert(Tree *tree, size_t key, size_t *info) {
        return OK;
     }
     if (cur) {
-        prev->right = temp;
-        temp->right = cur;
-        cur->parent = temp;
+        prev->relatives[RIGHT] = temp;
+        temp->relatives[RIGHT] = cur;
+        temp->relatives[PARENT] = prev;
+        cur->relatives[PARENT] = temp;
     } else {
-        if (key > prev->key) {
-            prev->right = temp;
-        } else {
-            prev->left = temp;
-        }
+        prev->relatives[index] = temp;
     }
     return OK;
 }
@@ -79,48 +122,41 @@ TreeStatus TreeKeyDelete(Tree *tree, size_t key) {
     if (!tree) {
         return NOT_EXIST;
     }
-    Node *target = NULL, *cur = tree->root;
+    Node *target = NULL;
     target = FindKey(tree, key);
     if (!target) {
         return NOT_FOUND;
     }
-
+    Node *parent = target->relatives[PARENT];
+    if (!target->relatives[LEFT] && !target->relatives[RIGHT]) {
+        parent->relatives[key < parent->key ? LEFT : RIGHT] = NULL;
+        NodeDelete(target);
+        return OK;
+    }
+    RelativeIndex index = LEFT; 
+    if (!target->relatives[LEFT] || !target->relatives[RIGHT]) {
+        if (!target->relatives[LEFT]) {
+            index = RIGHT;
+        }
+        target->relatives[index]->relatives[PARENT] = target->relatives[PARENT];
+        NodeDelete(target);
+        return OK;
+    }
+    Node *successor = FindNextKey(tree, key);
+    target->key = successor->key;
+    target->info = successor->info;
+    if (successor->relatives[PARENT]->relatives[LEFT] == successor) {
+        successor->relatives[PARENT]->relatives[LEFT] = successor->relatives[RIGHT];
+        if (successor->relatives[RIGHT]) {
+            successor->relatives[RIGHT]->relatives[PARENT] = successor->relatives[PARENT];
+        }
+    } else {
+        successor->relatives[PARENT]->relatives[RIGHT] = successor->relatives[RIGHT];
+        if (successor->relatives[RIGHT]) {
+            successor->relatives[RIGHT]->relatives[PARENT] = successor->relatives[PARENT];
+        }
+    }
+    NodeDelete(successor);
+    return OK;
 }
 
-/*
- * func delete(t : Node, v : Node):                 // t
-
- — дерево, v
-
- — удаляемый элемент
-   p = v.parent                                  // предок удаляемого элемента
-   if v.left == null and v.right == null         // первый случай: удаляемый элемент - лист
-     if p.left == v
-       p.left = null
-     if p.right == v
-       p.right = null
-   else if v.left == null or v.right == null     // второй случай: удаляемый элемент имеет одного потомка
-       if v.left == null                 
-           if p.left == v
-             p.left = v.right
-           else
-             p.right = v.right
-           v.right.parent = p 
-       else
-           if p.left == v
-               p.left = v.left
-           else
-               p.right = v.left
-           v.left.parent = p
-   else                                          // третий случай: удаляемый элемент имеет двух потомков
-     successor = next(v, t)                   
-     v.key = successor.key
-     if successor.parent.left == successor
-       successor.parent.left = successor.right
-       if successor.right != null
-         successor.right.parent = successor.parent
-     else
-       successor.parent.right = successor.right
-       if successor.right != null
-         successor.right.parent = successor.parent
-         */
